95f172ba338a30122956d10aa3e9e9428e033f42
Martin Hagerup <martin@zedge.net>
2017-07-20 16:56:49 +0200

    Fix compatibility issues with Swift 4, some type associations were not shadowed correctly

--- TSet.swift
+++ TSet.swift
@@ -19,94 +19,95 @@
 
 import Foundation
 
-public struct TSet<Element : TSerializable & Hashable> : SetAlgebra, Hashable, Collection, ExpressibleByArrayLiteral, TSerializable {
+public struct TSet<TElement : TSerializable & Hashable> : SetAlgebra, Hashable, Collection, ExpressibleByArrayLiteral, TSerializable {
+    
   /// Typealias for Storage type
-  typealias Storage = Set<Element>
+  public typealias Storage = Set<TElement>
   
   
-  /// Internal Storage used for TSet (Set\<Element\>)
+  /// Internal Storage used for TSet (Set\<TElement\>)
   internal var storage : Storage
   
   
   /// Mark: Collection
   
-  public typealias Indices = Storage.Indices
-  public typealias Index = Storage.Index
-  public typealias IndexDistance = Storage.IndexDistance
-  public typealias SubSequence = Storage.SubSequence
+  public typealias TIndices = Storage.Indices
+  public typealias TIndex = Storage.Index
+  public typealias TIndexDistance = Storage.IndexDistance
+  public typealias TSubSequence = Storage.SubSequence
   
   
-  public var indices: Indices { return storage.indices }
+  public var indices: TIndices { return storage.indices }
   
   // Must implement isEmpty even though both SetAlgebra and Collection provide it due to their conflciting default implementations
   public var isEmpty: Bool { return storage.isEmpty }
   
-  public func distance(from start: Index, to end: Index) -> IndexDistance {
+  public func distance(from start: TIndex, to end: TIndex) -> TIndexDistance {
     return storage.distance(from: start, to: end)
   }
   
-  public func index(_ i: Index, offsetBy n: IndexDistance) -> Index {
+  public func index(_ i: TIndex, offsetBy n: TIndexDistance) -> TIndex {
     return storage.index(i, offsetBy: n)
   }
   
-  public func index(_ i: Index, offsetBy n: IndexDistance, limitedBy limit: Index) -> Index? {
+  public func index(_ i: TIndex, offsetBy n: TIndexDistance, limitedBy limit: TIndex) -> TIndex? {
     return storage.index(i, offsetBy: n, limitedBy: limit)
   }
   
-  public subscript (position: Storage.Index) -> Element? {
+  public subscript (position: Set<TElement>.Index) -> TElement {
     return storage[position]
   }
   
   /// Mark: SetAlgebra
-  internal init(storage: Set<Element>) {
+  internal init(storage: Set<TElement>) {
     self.storage = storage
   }
   
-  public func contains(_ member: Element) -> Bool {
+  public func contains(_ member: TElement) -> Bool {
     return storage.contains(member)
   }
   
-  public mutating func insert(_ newMember: Element) -> (inserted: Bool, memberAfterInsert: Element) {
+  public mutating func insert(_ newMember: TElement) -> (inserted: Bool, memberAfterInsert: TElement) {
     return storage.insert(newMember)
   }
   
-  public mutating func remove(_ member: Element) -> Element? {
+  public mutating func remove(_ member: TElement) -> TElement? {
     return storage.remove(member)
   }
   
-  public func union(_ other: TSet<Element>) -> TSet {
+  public func union(_ other: TSet<TElement>) -> TSet {
     return TSet(storage: storage.union(other.storage))
   }
   
-  public mutating func formIntersection(_ other: TSet<Element>) {
+  public mutating func formIntersection(_ other: TSet<TElement>) {
     return storage.formIntersection(other.storage)
   }
   
-  public mutating func formSymmetricDifference(_ other: TSet<Element>) {
+  public mutating func formSymmetricDifference(_ other: TSet<TElement>) {
     return storage.formSymmetricDifference(other.storage)
   }
   
-  public mutating func formUnion(_ other: TSet<Element>) {
+  public mutating func formUnion(_ other: TSet<TElement>) {
     return storage.formUnion(other.storage)
   }
   
-  public func intersection(_ other: TSet<Element>) -> TSet {
+  public func intersection(_ other: TSet<TElement>) -> TSet {
     return TSet(storage: storage.intersection(other.storage))
   }
   
-  public func symmetricDifference(_ other: TSet<Element>) -> TSet {
+  public func symmetricDifference(_ other: TSet<TElement>) -> TSet {
     return TSet(storage: storage.symmetricDifference(other.storage))
   }
   
-  public mutating func update(with newMember: Element) -> Element? {
+  public mutating func update(with newMember: TElement) -> TElement? {
     return storage.update(with: newMember)
   }
   
   /// Mark: IndexableBase
   
-  public var startIndex: Index { return storage.startIndex }
-  public var endIndex: Index { return storage.endIndex }
-  public func index(after i: Index) -> Index {
+  public var startIndex: TIndex { return storage.startIndex }
+  public var endIndex: TIndex { return storage.endIndex }
+  public func index(after i: TIndex) -> TIndex {
     return storage.index(after: i)
   }
 
@@ -114,7 +115,7 @@ public struct TSet<Element : TSerializable & Hashable> : SetAlgebra, Hashable, C
     storage.formIndex(after: &i)
   }
   
-  public subscript(bounds: Range<Index>) -> SubSequence {
+  public subscript(bounds: Range<TIndex>) -> TSubSequence {
     return storage[bounds]
   }
 
@@ -136,23 +137,23 @@ public struct TSet<Element : TSerializable & Hashable> : SetAlgebra, Hashable, C
     storage = Storage()
   }
   
-  public init(arrayLiteral elements: Element...) {
+  public init(arrayLiteral elements: TElement...) {
     self.storage = Storage(elements)
   }
   
-  public init<Source : Sequence>(_ sequence: Source) where Source.Iterator.Element == Element {
+  public init<Source : Sequence>(_ sequence: Source) where Source.Iterator.Element == TElement {
     storage = Storage(sequence)
   }
   
   public static func read(from proto: TProtocol) throws -> TSet {
     let (elementType, size) = try proto.readSetBegin()
-    if elementType != Element.thriftType {
+    if elementType != TElement.thriftType {
       throw TProtocolError(error: .invalidData,
                            extendedError: .unexpectedType(type: elementType))
     }
     var set = TSet()
     for _ in 0..<size {
-      let element = try Element.read(from: proto)
+      let element = try TElement.read(from: proto)
       set.storage.insert(element)
     }
     try proto.readSetEnd()
@@ -160,9 +161,9 @@ public struct TSet<Element : TSerializable & Hashable> : SetAlgebra, Hashable, C
   }
   
   public func write(to proto: TProtocol) throws {
-    try proto.writeSetBegin(elementType: Element.thriftType, size: Int32(self.count))
+    try proto.writeSetBegin(elementType: TElement.thriftType, size: Int32(self.count))
     for element in self.storage {
-      try Element.write(element, to: proto)
+      try TElement.write(element, to: proto)
     }
     try proto.writeSetEnd()
   }
@@ -178,6 +179,7 @@ extension TSet: CustomStringConvertible, CustomDebugStringConvertible {
   
 }
 
-public func ==<Element>(lhs: TSet<Element>, rhs: TSet<Element>) -> Bool {
+public func ==<TElement>(lhs: TSet<TElement>, rhs: TSet<TElement>) -> Bool {
   return lhs.storage == rhs.storage
 }
+
